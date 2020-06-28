7e743ff61af2952a29ddefa32bc2154631ac1c28
Ubuntu <s13317555@gmail.com>
2020-06-28 03:47:11 +0000

    finished first stage interface construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homePage/homePage.component'
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-       <HomePage />
+      <HomePage />
     </div>
   );
 }
